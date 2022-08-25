20f4d89c94bf4e2c9c01bd62932699cdb7a8b589
EwersonBrandina <ewerson.brandina@hotmail.com.br>
2022-08-25 11:05:53 -0300

    Atualização 25/08/2022 - 11:05

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss','../explore-container/explore-container.component.scss']
 })
 export class Tab1Page {
-
+  
+  num1 : string
+  num2 : string
+  num3 : string
+  saida : string
+  
   constructor() {}
+
+analisar(){
+  var a = parseFloat(this.num1)
+  var b = parseFloat(this.num2)
+  var c = parseFloat(this.num3)
+  if ((a + b) > c && (a + c) > b && (b + c) > a){
+    if ((a == b) && (a == c) && (b == c)){
+      this.saida = "Equilatero"
+    }
+    if ((a != b) && (a != c) && (b != c)){
+      this.saida = "Escaleno"
+    }
+    if ((a == b && b != c) && (a == c && c != b) && (b == c && c != a)){
+      this.saida = "Isosceles"
+    }
+  }
+  else{
+    this.saida = "Triangulo Não Existe"
+  }
+}
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss','../explore-container/explore-container.component.scss']
 })
 export class Tab2Page {
 
